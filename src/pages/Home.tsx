d65344ec07a54cecdc8ebd421d6b2165a7a7f989
Danila Vavilov <DVavilov@example.com>
2024-01-04 13:33:17 -0800

    Second commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to MacOS Recorder</h1>
+            <p>Discover the ultimate recording solution for your MacOS.</p>
+            <a href="/src/pages/Recorder">Learn More</a>
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/pages/Recorder.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Recorder() {
+    return (
+        <div>
+            <h1>MacOS Recorder</h1>
+            <p>The ultimate solution for recording on your Mac.</p>
+            <h2>Features</h2>
+            <ul>
+                <li>Easy-to-use interface</li>
+                <li>High-quality recording</li>
+                <li>Screen and audio capture</li>
+                {/* Add more features based on the README */}
+            </ul>
+            <a href="https://github.com/TheOneStudioLLC/mac-recorder">Download Now</a>
+        </div>
+    );
+}
+
+export default Recorder;
