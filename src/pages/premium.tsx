9df3070e9a90a8c67ee72b342ea4942166c7f74d
Danila Vavilov <DVavilov@example.com>
2024-01-16 22:19:23 -0800

    Fixed build

---
+++ src/pages/premium.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import React from 'react';
+
+export default function Premium() {
+    return (
+        <div>
+            <h1 className="text-3xl text-red-500 font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+    );
+}
--- src/pages/releases.tsx
+++ src/pages/releases.tsx
@@ -1,1 +1,14 @@
 // https://github.com/TheOneStudioLLC/Mac-Recorder/releases
+
+import Link from 'next/link';
+import React from 'react';
+
+export default function Releases() {
+    return (
+        <div>
+            <h1 className="text-3xl text-red-500 font-bold underline">
+                Hello world!
+            </h1>
+        </div>
+    );
+}
